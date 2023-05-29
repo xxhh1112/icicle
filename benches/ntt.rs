12e45873e72e1549addf93549746b1e75703cda8
Vitalii <vitalii@ingonyama.com>
2023-05-29 11:46:15 +0200

    rust benches refactored

--- benches/ntt.rs
+++ benches/ntt.rs
@@ -2,39 +2,105 @@ extern crate criterion;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use icicle_utils::test_bls12_381::{interpolate_scalars_batch_bls12_381, interpolate_points_batch_bls12_381, set_up_scalars_bls12_381, set_up_points_bls12_381};
+use icicle_utils::test_bls12_381::{
+    interpolate_points_batch_bls12_381, interpolate_scalars_batch_bls12_381,
+    set_up_points_bls12_381, set_up_scalars_bls12_381, evaluate_points_batch_bls12_381, evaluate_scalars_batch_bls12_381,
+};
+use rustacuda::prelude::DeviceBuffer;
 
+const LOG_NTT_SIZES: [usize; 8] = [24, 26, 8, 9, 10, 11, 12, 18];
+const BATCH_SIZES: [usize; 8] = [1, 4, 8, 16, 256, 512, 1024, 1 << 14];
 
-const LOG_NTT_SIZES: [usize; 1] = [15];
-const BATCH_SIZES: [usize; 2] = [8, 16];
+const MAX_POINTS_LOG2: usize = 18;
+const MAX_SCALARS_LOG2: usize = 24;
 
-fn bench_point_ntt(c: &mut Criterion) {
+fn bench_ntt(c: &mut Criterion) {
     for log_ntt_size in LOG_NTT_SIZES {
         for batch_size in BATCH_SIZES {
             let ntt_size = 1 << log_ntt_size;
-            let (_, mut d_evals, mut d_domain) = set_up_points_bls12_381(ntt_size * batch_size, log_ntt_size, true);
 
-            c.bench_function(
-                &format!("EC NTT of size 2^{} in batch {}", log_ntt_size, batch_size),
-                |b| b.iter(|| interpolate_points_batch_bls12_381(&mut d_evals, &mut d_domain, batch_size))
+            bench_template(
+                MAX_POINTS_LOG2,
+                ntt_size,
+                batch_size,
+                log_ntt_size,
+                set_up_points_bls12_381,
+                evaluate_points_batch_bls12_381,
+                c,
+                "EC NTT",
+                false
+            );
+            bench_template(
+                MAX_POINTS_LOG2,
+                ntt_size,
+                batch_size,
+                log_ntt_size,
+                set_up_points_bls12_381,
+                interpolate_points_batch_bls12_381,
+                c,
+                "EC iNTT",
+                true
+            );
+            bench_template(
+                MAX_SCALARS_LOG2,
+                ntt_size,
+                batch_size,
+                log_ntt_size,
+                set_up_scalars_bls12_381,
+                evaluate_scalars_batch_bls12_381,
+                c,
+                "NTT",
+                false
+            );
+            bench_template(
+                MAX_SCALARS_LOG2,
+                ntt_size,
+                batch_size,
+                log_ntt_size,
+                set_up_scalars_bls12_381,
+                interpolate_scalars_batch_bls12_381,
+                c,
+                "iNTT",
+                true
             );
         }
     }
 }
 
-fn bench_scalar_ntt(c: &mut Criterion) {
-    for log_ntt_size in LOG_NTT_SIZES {
-        for batch_size in BATCH_SIZES {
-            let ntt_size = 1 << log_ntt_size;
-            let (_, mut d_evals, mut d_domain) = set_up_scalars_bls12_381(ntt_size * batch_size, log_ntt_size, true);
+fn bench_template<E, S>(
+    log_max_size: usize,
+    ntt_size: usize,
+    batch_size: usize,
+    log_ntt_size: usize,
+    set_data: fn(
+        test_size: usize,
+        log_domain_size: usize,
+        inverse: bool,
+    ) -> (Vec<E>, DeviceBuffer<E>, DeviceBuffer<S>),
+    bench_fn: fn(
+        d_evaluations: &mut DeviceBuffer<E>,
+        d_domain: &mut DeviceBuffer<S>,
+        batch_size: usize,
+    ) -> DeviceBuffer<E>,
+    c: &mut Criterion,
+    id: &str,
+    inverse: bool,
+) {
+    let count = ntt_size * batch_size;
 
-            c.bench_function(
-                &format!("Scalar NTT of size 2^{} in batch {}", log_ntt_size, batch_size),
-                |b| b.iter(|| interpolate_scalars_batch_bls12_381(&mut d_evals, &mut d_domain, batch_size))
-            );
-        }
+    let bench_id = &format!("{} of size 2^{} in batch {}", id, log_ntt_size, batch_size);
+
+    if count > 1 << log_max_size {
+        println!("Bench size exceeded: {}", bench_id);
+        return;
     }
+
+    let (_, mut d_evals, mut d_domain) = set_data(ntt_size * batch_size, log_ntt_size, inverse);
+
+    c.bench_function(&bench_id, |b| {
+        b.iter(|| bench_fn(&mut d_evals, &mut d_domain, batch_size))
+    });
 }
 
-criterion_group!(ntt_benches, bench_point_ntt, bench_scalar_ntt);
+criterion_group!(ntt_benches, bench_ntt);
 criterion_main!(ntt_benches);
